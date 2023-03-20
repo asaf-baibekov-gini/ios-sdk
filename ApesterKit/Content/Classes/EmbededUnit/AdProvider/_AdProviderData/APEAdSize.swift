d8d003a1955e0c4ed84e5c26051d42f79c086f9c
Asaf Baibekov <asafb@gini-apps.com>
2023-03-20 12:01:08 +0200

    cleanup APEAdSize

--- ApesterKit/Content/Classes/EmbededUnit/AdProvider/_AdProviderData/APEAdSize.swift
+++ ApesterKit/Content/Classes/EmbededUnit/AdProvider/_AdProviderData/APEAdSize.swift
@@ -7,14 +7,13 @@
 
 import Foundation
 
-internal enum APEAdSize : String , CustomStringConvertible
-{
+internal enum APEAdSize: String, CustomStringConvertible {
     case adSize320x50  = "Size: 320x50"
     case adSize300x250 = "Size: 300x250"
     
-    var size : CGSize {
+    var size: CGSize {
         switch self {
-        case .adSize320x50 : return .init(width: 320, height: 50)
+        case .adSize320x50: return .init(width: 320, height: 50)
         case .adSize300x250: return .init(width: 300, height: 250)
         }
     }
