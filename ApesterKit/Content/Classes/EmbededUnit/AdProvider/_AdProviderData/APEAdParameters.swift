a442b929987ced02ef460efce10f71c9d189ba48
Asaf Baibekov <asafb@gini-apps.com>
2023-03-20 11:59:52 +0200

    cleanup internal init?(from dictionary: [String: Any])

--- ApesterKit/Content/Classes/EmbededUnit/AdProvider/_AdProviderData/APEAdParameters.swift
+++ ApesterKit/Content/Classes/EmbededUnit/AdProvider/_AdProviderData/APEAdParameters.swift
@@ -7,29 +7,28 @@
 
 import Foundation
 
-internal protocol APEAdParameters
-{
-    var identifier : String    { get }
-    var isVariant  : Bool      { get }
-    var type       : APEAdType { get }
+internal protocol APEAdParameters {
+    var identifier: String { get }
+    var isVariant: Bool { get }
+    var type: APEAdType { get }
 }
 
-internal struct AdMobParams    : Hashable , APEAdParameters
-{
+internal struct AdMobParams: Hashable, APEAdParameters {
     internal let identifier: String
-    internal let isVariant : Bool
+    internal let isVariant: Bool
     internal let type: APEAdType
     
     internal init?(from dictionary: [String: Any]) {
-        
         typealias Keys = Constants.Monetization
         
-        guard let provider  = dictionary[Keys.adProvider] as? String , provider == Keys.adMob else { return nil }
-        guard let adUnitId  = dictionary[Keys.adUnitId  ] as? String else { return nil }
-        guard let typeStr   = dictionary[Keys.adType    ] as? String else { return nil }
-        guard let isVariant = dictionary[Keys.isVariant ] as? Bool   else { return nil }
-        
-        guard let adType    = APEAdType(rawValue: typeStr) else { return nil }
+        guard
+            let provider  = dictionary[Keys.adProvider] as? String,
+            let adUnitId  = dictionary[Keys.adUnitId  ] as? String,
+            let typeStr   = dictionary[Keys.adType    ] as? String,
+            let isVariant = dictionary[Keys.isVariant ] as? Bool,
+            let adType    = APEAdType(rawValue: typeStr),
+            provider == Keys.adMob
+        else { return nil }
         
         self.identifier = adUnitId
         self.isVariant = isVariant
@@ -37,37 +36,35 @@ internal struct AdMobParams    : Hashable , APEAdParameters
     }
 }
 
-internal struct PubMaticParams : Hashable , APEAdParameters
-{
+internal struct PubMaticParams: Hashable, APEAdParameters {
     internal let identifier: String
-    internal let isVariant : Bool
+    internal let isVariant: Bool
     internal let type: APEAdType
     
-    internal let profileId       : Int
-    internal let publisherId     : String
-    internal let appStoreUrl     : String
-    internal let appDomain       : String
-    internal let testMode        : Bool
-    internal let debugLogs       : Bool
-    internal let bidSummaryLogs  : Bool
-    internal let timeInView      : Int?
+    internal let profileId: Int
+    internal let publisherId: String
+    internal let appStoreUrl: String
+    internal let appDomain: String
+    internal let testMode: Bool
+    internal let debugLogs: Bool
+    internal let bidSummaryLogs: Bool
+    internal let timeInView: Int?
      
     internal init?(from dictionary: [String: Any]) {
-        
         typealias Keys = Constants.Monetization
         
-        guard let provider  = dictionary[Keys.adProvider] as? String , provider == Keys.pubMatic else { return nil }
-        guard let adUnitId  = dictionary[Keys.adUnitId  ] as? String else { return nil }
-        guard let typeStr   = dictionary[Keys.adType    ] as? String else { return nil }
-        guard let isVariant = dictionary[Keys.isVariant ] as? Bool   else { return nil }
-        
-        guard let adType    = APEAdType(rawValue: typeStr) else { return nil }
-        
-        guard let profileIdStr = dictionary[Keys.profileId  ] as? String , let profileId = Int(profileIdStr) else {
-            return nil
-        }
-        guard let appStoreUrl  = dictionary[Keys.appStoreUrl] as? String else { return nil }
-        guard let publisherId  = dictionary[Keys.publisherId] as? String else { return nil }
+        guard
+            let provider     = dictionary[Keys.adProvider] as? String,
+            let adUnitId     = dictionary[Keys.adUnitId] as? String,
+            let typeStr      = dictionary[Keys.adType] as? String,
+            let isVariant    = dictionary[Keys.isVariant] as? Bool,
+            let profileIdStr = dictionary[Keys.profileId] as? String,
+            let appStoreUrl  = dictionary[Keys.appStoreUrl] as? String,
+            let publisherId  = dictionary[Keys.publisherId] as? String,
+            let adType       = APEAdType(rawValue: typeStr),
+            let profileId    = Int(profileIdStr),
+            provider == Keys.pubMatic
+        else { return nil }
         
         self.identifier     = adUnitId
         self.isVariant      = isVariant
@@ -75,10 +72,10 @@ internal struct PubMaticParams : Hashable , APEAdParameters
         self.profileId      = profileId
         self.appStoreUrl    = appStoreUrl
         self.publisherId    = publisherId
-        self.appDomain      = dictionary[Keys.appDomain     ] as? String ?? ""
-        self.testMode       = dictionary[Keys.testMode      ] as? Bool ?? false
-        self.debugLogs      = dictionary[Keys.debugLogs     ] as? Bool ?? false
+        self.appDomain      = dictionary[Keys.appDomain] as? String ?? ""
+        self.testMode       = dictionary[Keys.testMode] as? Bool ?? false
+        self.debugLogs      = dictionary[Keys.debugLogs] as? Bool ?? false
         self.bidSummaryLogs = dictionary[Keys.bidSummaryLogs] as? Bool ?? false
-        self.timeInView     = dictionary[Keys.timeInView    ] as? Int
+        self.timeInView     = dictionary[Keys.timeInView] as? Int
     }
 }
